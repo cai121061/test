9a147d2b298525ae7f58830a79f12d8a24098371
cai121061 <cai121061@163.com>
2014-09-03 16:27:54 +0800

    test
    test

---
+++ test/test/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  test
+//
+//  Created by 才腾 on 14-9-3.
+//  Copyright (c) 2014年 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by 才腾 on 14-9-3.
+//  Copyright (c) 2014年 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
